6c8e43defd0711fd1e6da9c43d758919f885d2d3
zoethomas <zoethomas1540@gmail.com>
2020-11-04 10:43:42 +0000

    Updated database setup instructions

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,2 +1,2 @@
-# Create a new bookmarks table
+-- Create a new bookmarks table in both the bookmark_manager database and the bookmark_manager_test database
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
